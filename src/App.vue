7ffe7b64f2536fd36c251bcb92eb4974aa58904b
encore123 <ruskir53@gmail.com>
2020-07-09 18:54:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div><input v-model="base" /> Число</div>
+    <div><input v-model="howMany" /> Степень</div>
+    <template v-for="i in +howMany">
+      <Deg :base="+base" :exp="9+i" :key="i" />
+    </template>
+  </div>
+</template>
+
+<script>
+import Deg from "./Deg";
+export default {
+  name: "App",
+  components: {
+    Deg,
+  },
+  data() {
+    return {
+      base: 2,
+      howMany: 2,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.flex {
+  display: flex;
+}
+</style>
---
+++ src/Deg.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>{{ base }}</span
+    ><sup>{{ exp }}</sup> = {{ base ** exp }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Deg",
+  props: {
+    base: {
+      type: Number
+    },
+    exp: {
+      type: Number
+    }
+  }
+};
+</script>
+
+<style></style>
